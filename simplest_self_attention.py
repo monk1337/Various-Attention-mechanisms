aaa363e1c5df893e55489fa5cefa501dbce930c2
Aaditya Ura <3x3paul@gmail.com>
2019-11-20 17:59:50 +0530

    Update simplest_self_attention.py

--- simplest_self_attention.py
+++ simplest_self_attention.py
@@ -1,4 +1,6 @@
 
+#logits from any model (lstm, cnn or any ) or embedding 
+
 f_1 = tf.layers.conv1d(seq_fts, 1, 1)
 f_2 = tf.layers.conv1d(seq_fts, 1, 1)
 logits = f_1 + tf.transpose(f_2, [0, 2, 1])
