f9bafcae3cd4e13cbb2bf7a1c58e62b9a16a80d6
Aaditya Ura <3x3paul@gmail.com>
2019-11-20 17:59:03 +0530

    Create simplest_self_attention.py

---
+++ simplest_self_attention.py
@@ -0,0 +1,6 @@
+
+f_1 = tf.layers.conv1d(seq_fts, 1, 1)
+f_2 = tf.layers.conv1d(seq_fts, 1, 1)
+logits = f_1 + tf.transpose(f_2, [0, 2, 1])
+coefs = tf.nn.softmax(tf.nn.leaky_relu(logits) + bias_mat)
+
